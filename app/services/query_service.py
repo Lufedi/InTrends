439f0832f78cd564d00cf63a076bf12981eba438
Felipe Diaz <pipexir@gmail.com>
2019-04-14 21:12:11 -0500

    Log to query service added and nextrun param to scheduler added

--- app/services/query_service.py
+++ app/services/query_service.py
@@ -1,6 +1,7 @@
 from linkedin_api import Linkedin
 
 from app import db
+from app import app
 from models.Terms import Term
 from models.Jobs import Job
 
@@ -9,6 +10,7 @@ from models.Jobs import Job
 
 # Authenticate using any Linkedin account credentials
 def query_updates():
+    app.logger.info('running query updates')
     api = Linkedin('pipediaz94@hotmail.com', 'prueba1020')
     location_name = 'Estados Unidos'
     location_id = 'us:0'
@@ -21,5 +23,6 @@ def query_updates():
             'type': 'JOBS', 'query': 'search',
         })
         job = Job(location=location_id, total=_total, term=term)
+        app.logger.info(f'updated {term.term} : {_total}')
         db.session.add(job)
         db.session.commit()
