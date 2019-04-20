cc9f48bb9628dd2af819a32e46aaa89709f63819
Felipe Diaz <LuisFelipe.DiazChica@Honeywell.com>
2019-04-20 11:35:45 -0500

    postgresql

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app2.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
