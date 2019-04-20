91285387a92678f023e5eee605c94851912111f3
Felipe Diaz <LuisFelipe.DiazChica@Honeywell.com>
2019-04-20 10:36:34 -0500

    cron next run time

--- cron/cron.py
+++ cron/cron.py
@@ -6,7 +6,7 @@ from services.query_service import query_updates
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=query_updates, trigger="interval", seconds=3600)
+scheduler.add_job(func=query_updates, trigger="interval", seconds=3600, next_run_time=datetime.datetime.now())
 scheduler.start()
 
 # Shut down the scheduler when exiting the app
