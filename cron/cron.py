cc058fb312c4ebd0518751b2eb5f526382ac8f07
Felipe Diaz <pipexir@gmail.com>
2019-05-10 09:44:57 -0500

    up to 18000

--- cron/cron.py
+++ cron/cron.py
@@ -6,7 +6,7 @@ from services.query_service import query_updates
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=query_updates, trigger="interval", seconds=7200,) # 
+scheduler.add_job(func=query_updates, trigger="interval", seconds=18000,) # 
 scheduler.start()
 
 # Shut down the scheduler when exiting the app
